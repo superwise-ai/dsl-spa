5ca4da04d7d7f8656859b90045031fc372f9cffb
Brian Mcclannahan <brian.mcclannahan@askboss.ai>
2025-12-12 11:28:55 -0600

    Fixed bug in f-string.

--- src/dsl_spa/pipeline/pipeline.py
+++ src/dsl_spa/pipeline/pipeline.py
@@ -372,7 +372,7 @@ class CommandPipeline(Pipeline):
             else:
                 missing_fields = self.get_missing_fields_for_command(command_name)
                 if len(missing_fields) > 0:
-                    raise PipelineException(f"Missing fields: {", ".join(missing_fields)}")
+                    raise PipelineException(f"Missing fields: {', '.join(missing_fields)}")
                 else: 
                     raise PipelineException("Invalid command request")
                     
